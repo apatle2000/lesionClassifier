0d07cb8009f3341924a762d1cce968cf20a62c8f
aditya_patle <aditya.patle@livehealth.com>
2023-03-27 17:59:42 +0530

    clean git

---
+++ frontend/lesions/src/App.js
@@ -0,0 +1,16 @@
+import "./styleSheets/App.css";
+// import "";
+
+function App() {
+  return (
+    <div className="App">
+      <>
+      <div className='Body'>
+      <span>Hello</span>
+      </div>
+      </>
+    </div>
+  );
+}
+
+export default App;
