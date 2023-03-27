c9f12dbf94cc227462b061a1f39e1e85e0319ffc
aditya_patle <aditya.patle@livehealth.com>
2023-03-27 17:25:48 +0530

    setup commit

--- backend/lesionClassifier/lesionClassifier/wsgi.py
+++ backend/lesionClassifier/lesionClassifier/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for lesionClassifier project.
+WSGI config for lesionclassifier project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lesionClassifier.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lesionclassifier.settings')
 
 application = get_wsgi_application()
